195699e6b730726b8eac8fbd1aceb1f2ffa10da1
aaaa <aaaa@gmail.com>
2025-11-25 09:45:26 +0900

    Filter実装

--- src/main/java/com/example/app/config/ValidationConfig.java
+++ src/main/java/com/example/app/config/ValidationConfig.java
@@ -1,11 +1,14 @@
 package com.example.app.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.app.filter.AuthFilter;
 
 @Configuration
 public class ValidationConfig implements WebMvcConfigurer {
@@ -25,16 +28,16 @@ public class ValidationConfig implements WebMvcConfigurer {
 		return messageSource;
 	}
 	
-	/*
+	
 	// 認証用フィルタの有効化
 	@Bean
 	FilterRegistrationBean<AuthFilter> authFilter() {
 		var bean = new FilterRegistrationBean<AuthFilter>(new AuthFilter());
-		bean.addUrlPatterns("/members/*");
-		bean.addUrlPatterns("/news/*");
+		bean.addUrlPatterns("/admin/*");
+		// bean.addUrlPatterns("/rental/*");
 		return bean;
 	}
-	*/
+	
 	
 	/*
 	// uploadsフォルダをリソースとして利用可能にする
