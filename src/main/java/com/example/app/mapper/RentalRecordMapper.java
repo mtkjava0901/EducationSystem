b5b23bfab8d94525fd2f1e1c2a6eeee81604cf02
aaaa <aaaa@gmail.com>
2025-11-27 16:02:44 +0900

    Mapper(java/xml)追加

---
+++ src/main/java/com/example/app/mapper/RentalRecordMapper.java
@@ -0,0 +1,27 @@
+package com.example.app.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.app.domain.RentalRecord;
+
+@Mapper
+public interface RentalRecordMapper {
+
+	// 貸し出し履歴一覧
+	List<RentalRecord> selectAll();
+
+	// 貸し出し履歴1件取得
+	RentalRecord selectById(Integer id);
+
+	// 登録(借り出し)
+	void insert(RentalRecord record);
+
+	// 返却日時の更新
+	void updateReturnTime(RentalRecord record);
+
+	// 削除
+	void delete(Integer id);
+
+}
