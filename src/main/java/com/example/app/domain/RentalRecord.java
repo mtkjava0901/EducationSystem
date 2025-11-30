e17a30a097a0da761d4135ef3ae73c8f3d3e7ec1
mtk <mtkjava0901@gmail.com>
2025-11-30 19:49:48 +0900

    修正&B-04-03制作開始

---
+++ src/main/java/com/example/app/domain/RentalRecord.java
@@ -0,0 +1,18 @@
+package com.example.app.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data 
+public class RentalRecord { 
+	
+	private Integer id; //貸し出し記録のID 
+	
+	private Student student ; // 教材を借りた生徒のID //studentId(オブジェクト参照) 
+	
+	private Material material ; // 貸し出しをする教材のID //materialId(オブジェクト参照) 
+	
+	private LocalDateTime borrowedAt; // 教材が貸し出された日時 
+	private LocalDateTime returnedAt; // 教材が返却された日時 }
+}
